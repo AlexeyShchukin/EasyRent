99fb6fceede13340de5a6e91edaa7793cd3e6786
AlexeyShchukin <shchukin.developer@gmail.com>
2025-08-13 12:57:10 +0200

    feat: Add landlord query_param "my"

--- src/listing/views.py
+++ src/listing/views.py
@@ -11,6 +11,24 @@ class ListingViewSet(viewsets.ModelViewSet):
     serializer_class = ListingSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    def get_queryset(self):
+        """
+        If query param 'my' is present and True,
+        return only the current user's listings.
+        """
+        queryset = super().get_queryset()
+        current_user = self.request.user
+
+        is_authenticated = current_user.is_authenticated
+        is_landlord = current_user.groups.filter(name="Landlord").exists()
+
+        if is_authenticated and is_landlord:
+            my_param = self.request.query_params.get("my")
+            if my_param and my_param.lower() == "true":
+                return queryset.filter(owner=current_user)
+
+        return queryset
+
     def perform_create(self, serializer):
         """Associates the created object with the current user."""
         serializer.save(owner=self.request.user)
