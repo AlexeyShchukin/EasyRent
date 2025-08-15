f9e1e9fd727d6cae17289779272ab922f28b3143
AlexeyShchukin <shchukin.developer@gmail.com>
2025-08-15 15:45:16 +0200

    fix: Fix overlapping logic

--- src/listing/serializers/booking.py
+++ src/listing/serializers/booking.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from src.listing.models.booking import Booking
@@ -5,6 +7,8 @@ from src.listing.serializers.listing import ListingListSerializer
 
 
 class BookingCreateSerializer(serializers.ModelSerializer):
+    listing = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Booking
         fields = ('listing', 'start_date', 'end_date')
@@ -15,30 +19,41 @@ class BookingCreateSerializer(serializers.ModelSerializer):
         """
         start_date = data.get('start_date')
         end_date = data.get('end_date')
+        current_date = date.today()
 
-        if start_date and end_date and start_date > end_date:
+        if start_date < current_date:
+            raise serializers.ValidationError(
+                {"start_date": "Start date cannot be in the past."}
+            )
+
+        if start_date > end_date:
             raise serializers.ValidationError(
                 "End date must be after start date or same."
             )
 
-        listing = data.get('listing')
-        if listing:
-            overlapping_bookings = Booking.objects.filter(
-                listing=listing,
-                start_date__lte=end_date,
-                end_date__gte=start_date,
-                status__in=[
-                    Booking.BookingStatus.PENDING,
-                    Booking.BookingStatus.CONFIRMED
-                ]
-            ).exclude(
-                id=self.instance.id if self.instance else None
+        listing = self.context.get('listing')
+
+        if not listing:
+            raise serializers.ValidationError(
+                {"listing": "Listing object not provided in context."}
             )
 
-            if overlapping_bookings.exists():
-                raise serializers.ValidationError(
-                    "This listing is already booked for the selected dates."
-                )
+        overlapping_bookings = Booking.objects.filter(
+            listing=listing,
+            start_date__lte=end_date,
+            end_date__gte=start_date,
+            status__in=[
+                Booking.BookingStatus.PENDING,
+                Booking.BookingStatus.CONFIRMED
+            ]
+        ).exclude(
+            id=self.instance.id if self.instance else None
+        )
+
+        if overlapping_bookings.exists():
+            raise serializers.ValidationError(
+                "This listing is already booked for the selected dates."
+            )
 
         return data
 
