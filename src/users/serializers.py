67895ff2544357c8e1ea1fe1204e1e6f6a1bb850
AlexeyShchukin <shchukin.developer@gmail.com>
2025-08-17 17:47:01 +0200

    Feat: Add password validation

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
@@ -16,6 +18,14 @@ class RegisterSerializer(serializers.ModelSerializer):
             ('LANDLORD', 'Landlord')
         ),
         write_only=True
+    )
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
     class Meta:
@@ -35,6 +45,10 @@ class RegisterSerializer(serializers.ModelSerializer):
             'last_name': {'required': False},
         }
 
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
     def validate(self, data):
         if data['password'] != data['password_confirm']:
             raise serializers.ValidationError(
@@ -45,7 +59,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password_confirm')
         role = validated_data.pop('role')
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
 
         if role.lower() == 'landlord':
             group = Group.objects.get(name='Landlord')
