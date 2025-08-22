ffe0f2197856a4beb9ee31f751bb4c5194dbc53b
AlexeyShchukin <shchukin.developer@gmail.com>
2025-08-22 19:33:18 +0200

    feat: Add nginx

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Running migrations..."
-python manage.py migrate
-echo "Migrations completed"
-
-python manage.py ensure_groups
-python manage.py collectstatic --noinput
-
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
